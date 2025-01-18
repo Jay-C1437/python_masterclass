cfc370dfc53be5a790cc88fc131766c9fe3840bc
John Chris Matovu <jay427002@gmail.com>
2025-01-18 12:04:01 +0300

    if else
    if else

--- operations.py
+++ operations.py
@@ -1,5 +1,13 @@
-celsius = 32
+# celsius = 32
 
-fahrenheit = (celsius * 9/5) + 32
+# fahrenheit = (celsius * 9/5) + 32
 
-print(str(celsius) + " degrees celsius is equal to " + str(fahrenheit) + " degrees fahrenheit")
+# print(str(celsius) + " degrees celsius is equal to " + str(fahrenheit) + " degrees fahrenheit")
+
+num = 10
+if num > 0:
+    print("Positive")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative")
